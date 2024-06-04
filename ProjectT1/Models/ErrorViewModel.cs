8c0acc8eb2deca71e623d19ca185b02cd7d6b174
Raydberg Gabriel Chuquival Gil <raydga_2005@hotmail.com>
2024-06-03 19:25:09 -0500

    Agregar archivos de proyecto.

---
+++ ProjectT1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProjectT1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
